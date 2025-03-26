1093b6b276f95a1a62b17e7051b740e1dc173022
ojasks <ojaskrsingh24@gmail.com>
2025-03-27 01:33:30 +0530

    learned about conversion of datatypes

---
+++ 01_basics/01_conversionOperations.js
@@ -0,0 +1,21 @@
+// let score = "ojas"; //33  "33"  "33abc" null undefined true false ojas
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)// if we are only dealing with numeric values 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);// here it shows that "33abc" is converted to a number but it originally is NaN (not a number)
+
+//33 => 33
+//"33abc" => NaN // type is number but value is NaN
+//true =>1
+//
+
+let isLoggedIn = "ojas" // 1=> true  "" => false  "ojas"=>true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber); // ab dikhne me number jaisa hai vaise string ban gya hai
+console.log(stringNumber);j
+console.log(typeof stringNumber);
