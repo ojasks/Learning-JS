1b413536ee8713cba45550ba6b91c7f9cfb21496
ojasks <ojaskrsingh24@gmail.com>
2025-03-26 18:52:07 +0530

    Learnt about datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all JS code as the newer version
+//alert(3+3) // we are using nodejs not browser
+// code readability should be high
+let name="oJAS";
+let age=20;
+let isLoggedIn=false;
+let temp1 =null;//empty prefer null inplace of 0
+let temp2;//undefined
+
+//number >= 2 to power 53
+//bigint
+//string => ""
+// boolean => true and false
+// null => is a stand alone value as well as a data type
+// undefined =>
+//difference between undefined and null
+//symbol =>unique
+
+// object
+
+console.log(typeof "Ojas") //typeof and jis bhi value ka type jaan na ho
+console.log(typeof age); 
+console.log(typeof null); 
+console.log(typeof undefined); 
